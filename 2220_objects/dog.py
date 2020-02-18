3c5ed8d3b5c044770077c4bb7102a5ab9dcb1b8e
pablopd <pablopd@meta4.com>
2020-02-18 17:31:10 +0100

    dog class finished

--- 2220_objects/dog.py
+++ 2220_objects/dog.py
@@ -17,9 +17,54 @@ def bark(self):
     print("I aint got no dog!") 
 
 toby = Dog("Toby", 8, 12)
-toby.bark()
 print(toby)
+toby.bark()
 
 hotDog = Dog("HotDog", 10, 3)
-hotDog.bark()
 print(hotDog)
+hotDog.bark()
+
+
+class AssistanceDog(Dog):
+    def __init__(self, name, age, weight, owner):
+        Dog.__init__(self, name, age, weight)
+        self.owner = owner     
+        # private attribute (__ before name)
+        self.__working = False   
+    
+    def __str__(self):
+        return "assistance name: {}, with owner: {}".format(self.name, self.owner)
+    
+    def walk(self):        
+        print("walking with {} :)".format(self.owner))
+
+    def bark(self):
+        if self.__working == True:
+            print("I cant bark! Im working dude! ¬¬")  
+        else:
+            Dog.bark(self)
+
+    def working(self, value = None):
+        if value == None:
+            return self.__working
+        else:
+            self.__working = value
+
+
+helper = AssistanceDog("Santa´s Little Helper", 10, 8, "Homer")
+print(helper)
+helper.bark()
+helper.walk()
+# setter call
+helper.working(True)
+helper.bark()
+# setter call
+helper.working(False)
+helper.bark()
+# just fail, tha attribute is hidden, well not truly..
+# print(helper.__working)
+# you can counsult...
+print(helper._AssistanceDog__working)
+# Or.. correctly, use the getter
+# getter call
+print(helper.working())
