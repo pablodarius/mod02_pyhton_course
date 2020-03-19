819870b34a6b6bb69a651f95647dc83002fd8c70
pablodarius <pablodarius@hotmail.com>
2020-03-19 16:46:06 +0100

    finish the thermometer

--- 2440_Thermometer_pygame/mainApp.py
+++ 2440_Thermometer_pygame/mainApp.py
@@ -5,11 +5,48 @@ class Thermometer():
     def __init__(self):
         self.custome = pygame.image.load("./images/thermometer.png")
 
+    def convert(self, grades, toUnit):
+        result = 0
+        if toUnit == "F":
+            result = grades * 9/5 + 32
+        elif toUnit == "C":
+            result = (grades - 32) * 5/9
+        else:
+            result = grades
+
+        return "{:10.2f}".format(result)
+
+class Selector():
+    __unit = None
+
+    def __init__(self, unit= "C"):
+        self.__customes = []
+        self.__customes.append(pygame.image.load("./images/celsius.png"))
+        self.__customes.append(pygame.image.load("./images/farenheit.png"))
+        self.__unit = unit
+
+    def getUnit(self):
+        return self.__unit
+
+    def getCustome(self):
+        if self.__unit == "C":
+            return self.__customes[0]
+        elif self.__unit == "F":
+            return self.__customes[1]
+    
+    def change(self):        
+        if self.__unit == "C":
+            self.__unit = "F"
+        else:
+            self.__unit = "C"
+
+
 class NumberInput():
     __grades = 0
     __strValue = "0"
     __position = [0, 0]
     __size = [0, 0]
+    __pointsCount = 0
 
     def __init__(self, grades=0):
         self.__font = pygame.font.SysFont("Arial", 24)
@@ -18,10 +55,16 @@ class NumberInput():
 
     def on_event(self, event):
         if event.type == KEYDOWN:
-            if event.unicode in '01234567890' and len(self.__strValue) < 10:     # event.unicode.isdigit()  
+            if event.unicode.isdigit() and len(self.__strValue) < 10 or (event.unicode == '.' and self.__pointsCount == 0):
                 self.__strValue += event.unicode
-            elif event.key == K_BACKSPACE:
+                self.__grades = float(self.__strValue)
+                if event.unicode == '.':
+                    self.__pointsCount += 1
+            elif event.key == K_BACKSPACE and self.__strValue != '':
+                if self.__strValue[-1] == '.':
+                    self.__pointsCount = 0
                 self.__strValue = self.__strValue[:-1]
+                self.grades(self.__strValue)
 
     def render(self):
         textBlock = self.__font.render(self.__strValue, True, (74, 74, 74))
@@ -40,7 +83,7 @@ class NumberInput():
         else: 
             grad = str(grad)
             try:
-                self.__grades = int(grad)
+                self.__grades = float(grad)
                 self.__strValue = grad
             except:
                 pass
@@ -111,14 +154,14 @@ class mainApp():
 
     def __init__(self):
         self.__screen = pygame.display.set_mode((290, 415))
-        pygame.display.set_caption("Thermometer")
-        self.__screen.fill((244,236,203))
+        pygame.display.set_caption("Thermometer")        
         
         self.thermometer = Thermometer()        
         self.input = NumberInput()
         self.input.pos((106, 58))
         self.input.size((133, 28))
 
+        self.selector = Selector()
 
     def __onClose(self):
         pygame.quit()
@@ -130,13 +173,24 @@ class mainApp():
                 if event.type == QUIT:
                     self.__onClose()
                 
-                self.input.on_event(event)
-                        
+                self.input.on_event(event) 
+                if event.type == MOUSEBUTTONDOWN:
+                    self.selector.change() 
+                    grades = self.input.grades()        
+                    newUnit = self.selector.getUnit()           
+                    temperature = self.thermometer.convert(grades, newUnit)
+                    self.input.grades(temperature)
         
+            # backgorund
+            self.__screen.fill((244,236,203))
+            # thermometer
             self.__screen.blit(self.thermometer.custome, (50, 34))
+            # textbox
             text = self.input.render()
             pygame.draw.rect(self.__screen, (255, 255, 255), text["bg"])
             self.__screen.blit(text["text"], self.input.pos())
+            # selector
+            self.__screen.blit(self.selector.getCustome(), (112, 153))
 
             pygame.display.flip()
 
