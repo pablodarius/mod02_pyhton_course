306ef5fbf52791aba5b3601e34f8efd70daa5117
pablodarius <pablodarius@hotmail.com>
2020-03-22 21:03:52 +0100

    new folder tkinter

---
+++ 2610_Tkinter/example.py
@@ -0,0 +1,8 @@
+from tkinter import *
+ 
+root = Tk()
+ 
+w = Label(root, text="It comes as standard with Python")
+w.pack()
+ 
+root.mainloop()
---
+++ 2610_Tkinter/exercise.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+root.geometry("640x480")
+root.title("My Window")
+root.configure(bg = "pink")
+
+root.mainloop()
