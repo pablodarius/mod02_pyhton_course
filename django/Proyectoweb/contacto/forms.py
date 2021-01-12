3b20fb2a31ccaca678040a603a23fb29ee7a6599
pablodarius <pablodarius@hotmail.com>
2021-01-12 17:54:13 +0100

    videos 44 and 45 from course

--- django/Proyectoweb/contacto/forms.py
+++ django/Proyectoweb/contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContanto(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
     email = forms.EmailField(label="Email", required=True)
-    contenido = forms.CharField(label="Contenido")
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea)
--- django/Proyectoweb/contacto/views.py
+++ django/Proyectoweb/contacto/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContanto
 
 # Create your views here.
 def contacto(request):
     formulario_contacto = FormularioContanto()
+    if request.method == "POST":
+        formulario_contacto = FormularioContanto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            return redirect("/contacto/?valido")
+
     return render(request, "contacto/contacto.html", {'miFormulario': formulario_contacto})
